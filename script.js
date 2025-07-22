745326f2e173a328442c9d2786fc1d3d21bd6ae1
Chris <133912058+ch627110@users.noreply.github.com>
2025-07-21 20:01:40 -0400

    Create button to regenerate grid

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 // script.js
+const title = document.querySelector('#title')
 const container = document.querySelector('#container')
 
-// create a grid of 16x16 square divs using flexbox
-let val = 60;
-for(let row = 0; row < val; row++) { 
-    // create a horizontal flex box
-    const rowBox = document.createElement('div')
-    rowBox.classList.add('row')
-    container.appendChild(rowBox)
-    for(let col = 0; col < val; col++) {
-        // insert squares
-        const square = document.createElement('div')
-        square.classList.add('square')
-        square.addEventListener('mouseover', (event => {
-            square.setAttribute('style', 'background-color:black')
-        }))
-        rowBox.appendChild(square)
+createGrid(16);
+
+function createGrid(val) {
+    const grid = document.createElement('div')
+    grid.classList.add('grid');
+    container.appendChild(grid)
+    for(let row = 0; row < val; row++) { 
+        // create a horizontal flex box
+        const rowBox = document.createElement('div')
+        rowBox.classList.add('row')
+        grid.appendChild(rowBox)
+        for(let col = 0; col < val; col++) {
+            // insert squares
+            const square = document.createElement('div')
+            square.classList.add('square')
+            square.addEventListener('mouseover', (event => {
+                square.setAttribute('style', 'background-color:black')
+            }))
+            rowBox.appendChild(square)
+        }
     }
 }
